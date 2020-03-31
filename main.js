62511089a2b96d5a731330a56343be1a8081d254
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2020-03-31 17:12:59 -0300

    Increase window height

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron');
 function createWindow () {
     const win = new BrowserWindow({
         width: 1600,
-        height: 600,
+        height: 700,
         webPreferences: {
             nodeIntegration: true
         }
